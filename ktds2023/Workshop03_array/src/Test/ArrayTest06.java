c3cf1e95186f05a368fd4d198a78dbaf8c79cdaf
bigluck07 <bigluck0707@gmail.com>
2023-01-17 01:58:38 +0900

    1.16.

--- ktds2023/Workshop03_array/src/Test/ArrayTest06.java
+++ ktds2023/Workshop03_array/src/Test/ArrayTest06.java
@@ -1,20 +1,24 @@
 package Test;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayTest06 {
 
 	public static void main(String[] args) {
-//		다음 배열의 내용을 실행 결과와 같이 출력 되도록 프로그램을 작성 하시오.
-		int[][] arr2 = {
-				{20, 30, 10},
-				{50, 40, 60},
-				{80, 80, 90}
-				};
-		for (int i = 2; i >= 0; i--) {
-			for (int j = 2; j >= 0; j--) {
-				System.out.print(arr2[i][j]+" ");
-			}
+//		Scanner 클래스를 사용하여 키보드로 키(height)값을 입력 받아서 
+//		최대값을 구하는 코드 작 업. 입력 받은 사람 수만큼 배열을 생성하여 사용한다.
+		System.out.println("키의 최댓값을 구합니다.");
+		Scanner scner = new Scanner(System.in);
+		int num = scner.nextInt();
+		System.out.println("사람 수: "+num);
+		int max_high = 0;
+		for (int i = 1; i < num+1; i++) {
+			int high = scner.nextInt();
+			System.out.println("사람 "+i+": "+high);
+			if (max_high < high) max_high=high;
 		}
+		System.out.println("최댓값은 "+max_high+"입니다.");
+		
 	}//end main
 
 }//end class
